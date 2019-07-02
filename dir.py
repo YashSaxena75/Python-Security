6002806aec59a9e55dbece4adafce6661e5a6884
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-07-02 15:54:19 +0530

    Update dir.py

--- dir.py
+++ dir.py
@@ -1,17 +1,15 @@
 import requests
-url="dituniversity.edu.in"
-
+url="kvkashipur.com"
 def req(url):
-	try:
-		return requests.get("http://" + url)
-	except Exception ,e:
-		pass
+        try:
+                return requests.get("http://" + url)
+        except Exception ,e:
+                print("[-] No Repsonse By -->" + url)
 
 with open("/root/Downloads/sub.txt","r") as f:
-	for x in f:
-		word=x.strip()
-		turl=word + "." + url
-		resp=req(turl)
-		if resp:
-			print("[+] Discovered subdomain --> " + turl + "\t\t" + str(requests.get("http://" + turl)))
-
+        for x in f:
+                word=x.strip()
+                turl=url + "/" + word
+                resp=req(turl)
+                if resp:
+                        print("[+] Discovered URL --> " + turl + "\t\t\t\t\t\t\t" + str(requests.get("http://" + turl)))
